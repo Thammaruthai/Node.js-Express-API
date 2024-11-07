1c39225b72091a7ceb7e0251d4ebd785e5bf2ca3
Thammaruthai <thammaruthai@gmail.com>
2024-11-07 14:10:36 +0700

    feat: add API server

---
+++ app.mjs
@@ -0,0 +1,22 @@
+import express from "express";
+import cors from "cors";
+
+const app = express();
+const port = 4000;
+
+const profile = {
+  name: "john",
+  age: 20,
+};
+
+// open port for API
+app.listen(port, () => {
+  console.log(`Server starting at ${port}`);
+});
+
+app.use(cors());
+
+// Get profile and Response Profile
+app.get("/profiles", (req, res) => {
+  return res.json(profile);
+});
